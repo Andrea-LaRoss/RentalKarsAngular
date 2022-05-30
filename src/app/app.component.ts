a2a5e7bc95baebea0e775a05d7f003e98783e6c0
Andrea-LaRoss <andrea.199020@gmail.com>
2022-05-30 15:58:18 +0200

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ButtonConfig} from "./button-config";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'RentalKarsAngular';
+  title = 'RentalKars';
+
+  bottone: ButtonConfig = {
+    css: "btn btn-primary",
+    text:"Testing 1",
+    type:"button"
+  };
+
+  bottone2: ButtonConfig = {
+    css: "btn btn-success",
+    text:"Testing 2",
+    type:"button"
+  };
+
+  test1(): void {
+    console.log("Funzione 1");
+  }
+
+  test2(): void {
+    console.log("Funzione 2");
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ConfigurableButtonComponent } from './configurable-button/configurable-button.component';
+import { HeaderComponent } from './header/header.component';
+import { TableTemplateComponent } from './table-template/table-template.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConfigurableButtonComponent,
+    HeaderComponent,
+    TableTemplateComponent
   ],
   imports: [
     BrowserModule
