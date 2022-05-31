17424efcda7b9479b679de050e2f0e0210c5d564
Andrea-LaRoss <andrea.199020@gmail.com>
2022-05-31 12:07:45 +0200

    Commit

---
+++ src/app/mock-users.ts
@@ -0,0 +1,12 @@
+import { User } from './user';
+
+export const USERS: User[] = [
+
+  {email: "test@test.com", password: "", firstName: "Andrea", lastName: "La Rosa"},
+  {email: "test@test.com", password: "", firstName: "John", lastName: "Pieri"},
+  {email: "test@test.com", password: "", firstName: "Al", lastName: "Bauli"},
+  {email: "test@test.com", password: "", firstName: "Jack", lastName: "La Rosa"},
+  {email: "test@test.com", password: "", firstName: "Andrea", lastName: "La Rosa"},
+  {email: "test@test.com", password: "", firstName: "Andrea", lastName: "La Rosa"},
+
+];
