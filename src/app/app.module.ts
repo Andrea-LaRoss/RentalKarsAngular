aa6027604d938e98c4b07fc2e836e812dfa3820d
Andrea-LaRoss <andrea.199020@gmail.com>
2022-06-06 13:33:29 +0200

    Da sistemare

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ConfigurableButtonComponent } from './configurable-button/configurable-button.component';
+import { ConfigurableButtonComponent } from './button-template/configurable-button.component';
 import { HeaderComponent } from './header/header.component';
 import { TableTemplateComponent } from './table-template/table-template.component';
 
