b9f02be14c4ac4169137876e6075761dfe32889e
Andrea-LaRoss <andrea.199020@gmail.com>
2022-06-07 17:47:12 +0200

    Ordinate Cartelle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { CarsListComponent } from './components/cars-list/cars-list.component';
 import { UsersListComponent } from './components/users-list/users-list.component';
 import { RentListComponent } from './components/rent-list/rent-list.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { UserFormComponent } from './components/forms/user-form/user-form.component';
+import { ReservationFormComponent } from './components/forms/reservation-form/reservation-form.component';
+import { CarFormComponent } from './components/forms/car-form/car-form.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +25,10 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
     CarsListComponent,
     UsersListComponent,
     RentListComponent,
-    DashboardComponent
+    DashboardComponent,
+    UserFormComponent,
+    ReservationFormComponent,
+    CarFormComponent
   ],
 
   imports: [
@@ -32,7 +38,10 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
       {path: 'cars', component: CarsListComponent},
       {path: 'users', component: UsersListComponent},
       {path: 'reservations', component: RentListComponent},
-      {path: 'dashboard', component: DashboardComponent}])
+      {path: 'dashboard', component: DashboardComponent},
+      {path: 'user_form', component: UserFormComponent},
+      {path: 'reservation_form', component: ReservationFormComponent},
+      {path: 'car_form', component: CarFormComponent}])
   ],
   providers: [],
   bootstrap: [AppComponent]
